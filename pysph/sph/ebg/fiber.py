83f3360c5206c41d2a3110db0d445a86ba07f502
Nils Meyer <mail@meyernils.de>
2017-05-24 20:36:20 +0200

    Refactored fiber EBG.

--- pysph/sph/ebg/fiber.py
+++ pysph/sph/ebg/fiber.py
@@ -11,14 +11,14 @@ class HoldPoints(Equation):
     r"""**Holds Flagged Points **
 
     Points tagged with 'tag' are excluded from accelaration. This little trick allows
-    testing of EBGs with fixed BCs. 
+    testing of Element Bending Groups with fixed BCs. 
     """
 
-    def __init__(self, dest, sources, tags, x=True, y=True, z=True):
+    def __init__(self, dest, sources, tag, x=True, y=True, z=True):
         r"""
         Parameters
         ----------
-        tags : list(float)
+        tags : int
             tag of fixed particle
         x : boolean
             True, if x-position should not be changed
@@ -27,23 +27,29 @@ class HoldPoints(Equation):
         z : boolean
             True, if z-position should not be changed
         """
-        self.tags = tags
+        self.tag = tag
         self.x = x
         self.y = y
         self.z = z
         super(HoldPoints, self).__init__(dest, sources)
 
-    def loop(self, d_idx, d_tag, d_au, d_av, d_aw):
-        if d_tag[d_idx] in self.tags :
-            if self.x : d_au[d_idx] = 0 
-            if self.y : d_av[d_idx] = 0
-            if self.z : d_aw[d_idx] = 0
+    def loop(self, d_idx, d_tag, d_au, d_av, d_aw, d_auhat, d_avhat, d_awhat):
+        if d_tag[d_idx] == self.tag :
+            if self.x :
+                d_au[d_idx] = 0 
+                d_auhat[d_idx] = 0
+            if self.y :
+                d_av[d_idx] = 0
+                d_avhat[d_idx] = 0
+            if self.z :
+                d_aw[d_idx] = 0
+                d_awhat[d_idx] = 0
 
 class Tension(Equation):
     r"""**Linear elastic fiber tension**
 
-    Particle acceleration based on fiber tension is computed. First particle in fiber 
-    must be tagged with 100 and last one must be tagged with -100.
+    Particle acceleration based on fiber tension is computed. The source particles must be 
+    chosen to be the same as the destination particles 
     """
 
     def __init__(self, dest, sources, ea):
@@ -60,30 +66,22 @@ class Tension(Equation):
         d_au[d_idx] = 0.0
         d_av[d_idx] = 0.0
         d_aw[d_idx] = 0.0
-    
-    def loop(self, d_idx, d_tag, d_m, d_lprev, d_lnext,
-                d_x, d_y, d_z, d_au, d_av, d_aw):
+
+    def loop(self, d_idx, s_idx, d_m, d_lprev, d_lnext,
+             d_au, d_av, d_aw, XIJ, RIJ):
         # interaction with previous particle
-        if not d_tag[d_idx] == 100:
-            Dx = d_x[d_idx]-d_x[d_idx-1]
-            Dy = d_y[d_idx]-d_y[d_idx-1]
-            Dz = d_z[d_idx]-d_z[d_idx-1]
-            l = sqrt(Dx**2+Dy**2+Dz**2)
-            t = self.ea*(l/d_lprev[d_idx]-1)
-            d_au[d_idx] -= (t*Dx/l)/d_m[d_idx]
-            d_av[d_idx] -= (t*Dy/l)/d_m[d_idx]
-            d_aw[d_idx] -= (t*Dz/l)/d_m[d_idx]
+        if d_idx == s_idx+1:
+            t = self.ea*(RIJ/d_lprev[d_idx]-1)
+            d_au[d_idx] -= (t*XIJ[0]/RIJ)/d_m[d_idx]
+            d_av[d_idx] -= (t*XIJ[1]/RIJ)/d_m[d_idx]
+            d_aw[d_idx] -= (t*XIJ[2]/RIJ)/d_m[d_idx]
 
         # interaction with next particle
-        if not d_tag[d_idx] == -100:
-            Dx = d_x[d_idx]-d_x[d_idx+1]
-            Dy = d_y[d_idx]-d_y[d_idx+1]
-            Dz = d_z[d_idx]-d_z[d_idx+1]
-            l = sqrt(Dx**2+Dy**2+Dz**2)
-            t = self.ea*(l/d_lprev[d_idx]-1)
-            d_au[d_idx] -= (t*Dx/l)/d_m[d_idx]
-            d_av[d_idx] -= (t*Dy/l)/d_m[d_idx]
-            d_aw[d_idx] -= (t*Dz/l)/d_m[d_idx]
+        if d_idx == s_idx-1:
+            t = self.ea*(RIJ/d_lnext[d_idx]-1)
+            d_au[d_idx] -= (t*XIJ[0]/RIJ)/d_m[d_idx]
+            d_av[d_idx] -= (t*XIJ[1]/RIJ)/d_m[d_idx]
+            d_aw[d_idx] -= (t*XIJ[2]/RIJ)/d_m[d_idx]
 
 class Damping(Equation):
     r"""**Damp particle motion**
@@ -115,8 +113,8 @@ class Damping(Equation):
 class Bending(Equation):
     r"""**Linear elastic fiber bending**
 
-    Particle acceleration based on fiber bending is computed. First particle in fiber 
-    must be tagged with 100 and last one must be tagged with -100.
+    Particle acceleration based on fiber bending is computed. The source particles must be 
+    chosen to be the same as the destination particles 
 
     """
 
@@ -135,28 +133,42 @@ class Bending(Equation):
         d_av[d_idx] = 0.0
         d_aw[d_idx] = 0.0
 
-    def loop(self, d_idx, d_tag, d_m, d_phi0,
-                d_x, d_y, d_z, d_au, d_av, d_aw):
-        # interaction with previous particle
-        if not d_tag[d_idx] == 100 and not d_tag[d_idx] == -100:
-            # vector to previous particle
-            xab = d_x[d_idx-1]-d_x[d_idx]
-            yab = d_y[d_idx-1]-d_y[d_idx]
-            zab = d_z[d_idx-1]-d_z[d_idx]
-            # vector to next particle
-            xbc = d_x[d_idx+1]-d_x[d_idx]
-            ybc = d_y[d_idx+1]-d_y[d_idx]
-            zbc = d_z[d_idx+1]-d_z[d_idx]
+    def loop(self, d_idx, s_idx, d_rxnext, d_rynext, d_rznext, d_rnext,
+                d_rxprev, d_ryprev, d_rzprev, d_rprev, XIJ, RIJ):
+        '''The loop saves vectors to previous and next particle only.'''
+        if d_idx == s_idx+1:
+            d_rxprev[d_idx] = XIJ[0]
+            d_ryprev[d_idx] = XIJ[1]
+            d_rzprev[d_idx] = XIJ[2]
+            d_rprev[d_idx] = RIJ
+        if d_idx == s_idx-1:
+            d_rxnext[d_idx] = XIJ[0]
+            d_rynext[d_idx] = XIJ[1]
+            d_rznext[d_idx] = XIJ[2]
+            d_rnext[d_idx] = RIJ
 
-            # norms
-            rab = sqrt(xab**2+yab**2+zab**2)
-            rbc = sqrt(xbc**2+ybc**2+zbc**2)
+    def post_loop(self, d_idx, d_tag, d_m, d_phi0,
+                d_rxnext, d_rynext, d_rznext, d_rnext,
+                d_rxprev, d_ryprev, d_rzprev, d_rprev,
+                d_au, d_av, d_aw):
+        if d_rnext[d_idx] > 1E-14 and d_rprev[d_idx] > 1E-14:
+            # vector to previous particle
+            xab = d_rxprev[d_idx]
+            yab = d_ryprev[d_idx]
+            zab = d_rzprev[d_idx]
+            rab = d_rprev[d_idx]
+            # vector to next particle
+            xbc = d_rxnext[d_idx]
+            ybc = d_rynext[d_idx]
+            zbc = d_rznext[d_idx]
+            rbc = d_rnext[d_idx]
+
             # normed dot product between vectors (limited to catch round off errors)
-            dot_prod = (xab*xbc+yab*ybc+zab*zbc)/(rab*rbc)
-            dot_prod = max(-1, dot_prod)
-            dot_prod = min(1, dot_prod)
+            dot_prod_norm = (xab*xbc+yab*ybc+zab*zbc)/(rab*rbc)
+            dot_prod_norm = max(-1, dot_prod_norm)
+            dot_prod_norm = min(1, dot_prod_norm)
             # angle between vectors
-            phi = acos(dot_prod)
+            phi = acos(dot_prod_norm)
             # direction of angle from cross product
             norm = rab*rbc*sin(phi)
             nx = (yab*zbc-zab*ybc)/norm
@@ -169,12 +181,12 @@ class Bending(Equation):
             Mz = 2*self.ei*(phi-d_phi0[d_idx])/(rab+rbc)*nz
 
             # forces on neighbouring particles
-            Fabx = -(My*zab-Mz*yab)/(rab**2)
-            Faby = -(Mz*xab-Mx*zab)/(rab**2)
-            Fabz = -(Mx*yab-My*xab)/(rab**2)
-            Fbcx = (My*zbc-Mz*ybc)/(rbc**2)
-            Fbcy = (Mz*xbc-Mx*zbc)/(rbc**2)
-            Fbcz = (Mx*ybc-My*xbc)/(rbc**2)
+            Fabx = (My*zab-Mz*yab)/(rab**2)
+            Faby = (Mz*xab-Mx*zab)/(rab**2)
+            Fabz = (Mx*yab-My*xab)/(rab**2)
+            Fbcx = -(My*zbc-Mz*ybc)/(rbc**2)
+            Fbcy = -(Mz*xbc-Mx*zbc)/(rbc**2)
+            Fbcz = -(Mx*ybc-My*xbc)/(rbc**2)
 
             d_au[d_idx] += (Fabx+Fbcx)/d_m[d_idx]
             d_av[d_idx] += (Faby+Fbcy)/d_m[d_idx]
