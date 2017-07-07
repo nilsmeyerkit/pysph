37cfae39b08da4a06f09a26b6760b6356281ca5e
Nils Meyer <mail@meyernils.de>
2017-07-07 13:50:45 -0400

    Created a new 3D friction model. Full physical motivation, no coefficient necessary.

--- pysph/sph/ebg/fiber.py
+++ pysph/sph/ebg/fiber.py
@@ -5,7 +5,7 @@ Element Bending Group
 """
 
 from pysph.sph.equation import Equation
-from math import sqrt, acos, sin, pi, floor
+from math import sqrt, acos, atan, sin, pi, floor
 
 class HoldPoints(Equation):
     r"""**Holds Flagged Points **
@@ -70,6 +70,41 @@ class Vorticity(Equation):
         d_omegax[d_idx] += v*(VIJ[1]*DWIJ[2]-VIJ[2]*DWIJ[1])
         d_omegay[d_idx] += v*(VIJ[2]*DWIJ[0]-VIJ[0]*DWIJ[2])
         d_omegaz[d_idx] += v*(VIJ[0]*DWIJ[1]-VIJ[1]*DWIJ[0])
+
+class VelocityGradient(Equation):
+    r"""** Computes 2nd order tensor representing the velocity gradient**
+    """
+
+    def initialize(self, d_idx, d_dudx, d_dudy, d_dudz, d_dvdx, d_dvdy, d_dvdz,
+                    d_dwdx, d_dwdy, d_dwdz):
+        d_dudx[d_idx] = 0.0
+        d_dudy[d_idx] = 0.0
+        d_dudz[d_idx] = 0.0
+
+        d_dvdx[d_idx] = 0.0
+        d_dvdy[d_idx] = 0.0
+        d_dvdz[d_idx] = 0.0
+
+        d_dwdx[d_idx] = 0.0
+        d_dwdy[d_idx] = 0.0
+        d_dwdz[d_idx] = 0.0
+
+
+    def loop(self, d_idx, s_idx, d_rho, s_m, d_dudx, d_dudy, d_dudz, d_dvdx,
+            d_dvdy, d_dvdz,d_dwdx, d_dwdy, d_dwdz, DWIJ, VIJ):
+        v = s_m[s_idx]/d_rho[d_idx]
+        d_dudx[d_idx] -= v*VIJ[0]*DWIJ[0]
+        d_dudy[d_idx] -= v*VIJ[0]*DWIJ[1]
+        d_dudz[d_idx] -= v*VIJ[0]*DWIJ[2]
+
+        d_dvdx[d_idx] -= v*VIJ[1]*DWIJ[0]
+        d_dvdy[d_idx] -= v*VIJ[1]*DWIJ[1]
+        d_dvdz[d_idx] -= v*VIJ[1]*DWIJ[2]
+
+        d_dwdx[d_idx] -= v*VIJ[2]*DWIJ[0]
+        d_dwdy[d_idx] -= v*VIJ[2]*DWIJ[1]
+        d_dwdz[d_idx] -= v*VIJ[2]*DWIJ[2]
+
 
 class Tension(Equation):
     r"""**Linear elastic fiber tension**
@@ -224,136 +259,250 @@ class Bending(Equation):
             d_av[d_idx-1] -= Faby/d_m[d_idx-1]
             d_aw[d_idx-1] -= Fabz/d_m[d_idx-1]
 
-#class Friction(Equation):
-#    r"""**Fiber bending based on friction**#
-#
-#    Particle acceleration based on fiber bending is computed. The source
-#    particles must be chosen to be the same as the destination particles
-#    """
-#
-#    def __init__(self, dest, sources, k):
-#        r"""
-#        Parameters
-#        ----------
-#        k : float
-#            friction coefficient for torque from vorticity
-#        """
-#        self.k = k
-#        super(Friction, self).__init__(dest, sources)
-#
-#    def initialize(self, d_idx, d_au, d_av, d_aw):
-#        d_au[d_idx] = 0.0
-#        d_av[d_idx] = 0.0
-#        d_aw[d_idx] = 0.0
-#
-#    def loop(self, d_idx, s_idx, d_rxnext, d_rynext, d_rznext, d_rnext,
-#                d_rxprev, d_ryprev, d_rzprev, d_rprev, XIJ, RIJ):
-#        '''The loop saves vectors to previous and next particle only.'''
-#        if d_idx == s_idx+1:
-#            d_rxprev[d_idx] = XIJ[0]
-#            d_ryprev[d_idx] = XIJ[1]
-#            d_rzprev[d_idx] = XIJ[2]
-#            d_rprev[d_idx] = RIJ
-#        if d_idx == s_idx-1:
-#            d_rxnext[d_idx] = XIJ[0]
-#            d_rynext[d_idx] = XIJ[1]
-#            d_rznext[d_idx] = XIJ[2]
-#            d_rnext[d_idx] = RIJ
-#
-#    def post_loop(self, d_idx, d_m,
-#                d_rxnext, d_rynext, d_rznext, d_rnext,
-#                d_rxprev, d_ryprev, d_rzprev, d_rprev,
-#                d_au, d_av, d_aw, d_omegax, d_omegay, d_omegaz):
-#        if d_rnext[d_idx] > 1E-14 and d_rprev[d_idx] > 1E-14:
-#            # vector to previous particle
-#            xab = d_rxprev[d_idx]
-#            yab = d_ryprev[d_idx]
-#            zab = d_rzprev[d_idx]
-#            rab = d_rprev[d_idx]
-#            # vector to next particle
-#            xbc = d_rxnext[d_idx]
-#            ybc = d_rynext[d_idx]
-#            zbc = d_rznext[d_idx]
-#            rbc = d_rnext[d_idx]
-#
-#            # Averaged vorticity
-#            #ox = (d_omegax[d_idx-1]+d_omegax[d_idx]+d_omegax[d_idx+1])/3
-#            #oy = (d_omegay[d_idx-1]+d_omegay[d_idx]+d_omegay[d_idx+1])/3
-#            #oz = (d_omegaz[d_idx-1]+d_omegaz[d_idx]+d_omegaz[d_idx+1])/3
-#            ox = d_omegax[d_idx]
-#            oy = d_omegay[d_idx]
-#            oz = d_omegaz[d_idx]
-#
-#            kabx = self.k*(oy*zab-oz*yab)/(rab**2)
-#            kaby = self.k*(oz*xab-ox*zab)/(rab**2)
-#            kabz = self.k*(ox*yab-oy*xab)/(rab**2)
-#            kbcx = -self.k*(oy*zbc-oz*ybc)/(rbc**2)
-#            kbcy = -self.k*(oz*xbc-ox*zbc)/(rbc**2)
-#            kbcz = -self.k*(ox*ybc-oy*xbc)/(rbc**2)
-#
-#            d_au[d_idx+1] += kbcx/d_m[d_idx+1]
-#            d_av[d_idx+1] += kbcy/d_m[d_idx+1]
-#            d_aw[d_idx+1] += kbcz/d_m[d_idx+1]
-#            d_au[d_idx-1] -= kabx/d_m[d_idx-1]
-#            d_av[d_idx-1] -= kaby/d_m[d_idx-1]
-#            d_aw[d_idx-1] -= kabz/d_m[d_idx-1]
 
 class Friction(Equation):
-    r"""**Fiber bending based on friction**
+    r"""**Fiber bending based on friction**#
 
-    Rigid rotation based on friction. Does not work across periodic boundaries!
+    .... The source
+    particles must be chosen to be the same as the destination particles
     """
 
-    def __init__(self, dest, sources, k, n):
+    def __init__(self, dest, sources, J, A, nu, d, ar):
         r"""
         Parameters
         ----------
-        k : float
-            friction coefficient for torque from vorticity
-        n : int
-            rigid motion rod length
+        J : float
+            moment of inertia
+        A : float
+            shell surface area (2D: 2*dx and 3D: dx*pi*d/2)
+        mu : float
+            viscosity
         """
-        self.k = k
-        self.N = n-1
+        self.J = J
+        self.A = A
+        self.nu = nu
+        self.d = d
+        self.ar = ar
         super(Friction, self).__init__(dest, sources)
 
-    def initialize(self, d_idx, d_au, d_av, d_aw):
-        d_au[d_idx] = 0.0
-        d_av[d_idx] = 0.0
-        d_aw[d_idx] = 0.0
+    def initialize(self, d_idx, d_au, d_av, d_aw, d_testx, d_testy, d_testz):
+       d_au[d_idx] = 0.0
+       d_av[d_idx] = 0.0
+       d_aw[d_idx] = 0.0
+       d_testx[d_idx] = 0.0
+       d_testy[d_idx] = 0.0
+       d_testz[d_idx] = 0.0
 
-    def loop(self, d_idx, d_x, d_y, d_z, d_omegax, d_omegay, d_omegaz,
-                d_au, d_av, d_aw, d_m, d_tag, d_testx, d_testy, d_testz):
+    def loop(self, d_idx, s_idx, d_rxnext, d_rynext, d_rznext, d_rnext,
+                d_rxprev, d_ryprev, d_rzprev, d_rprev, d_x, d_y, d_z, XIJ, RIJ):
+        '''The loop saves vectors to previous and next particle only.'''
+        if d_idx == s_idx+1:
+            d_rxprev[d_idx] = XIJ[0]
+            d_ryprev[d_idx] = XIJ[1]
+            d_rzprev[d_idx] = XIJ[2]
+            d_rprev[d_idx] = RIJ
+        if d_idx == s_idx-1:
+            d_rxnext[d_idx] = XIJ[0]
+            d_rynext[d_idx] = XIJ[1]
+            d_rznext[d_idx] = XIJ[2]
+            d_rnext[d_idx] = RIJ
 
-        if ((d_idx+1)%(self.N+1) == 0 and d_idx > 0):
-            xx = 0.0; yy = 0.0; zz = 0.0
-            ox = 0.0; oy = 0.0; oz = 0.0
-            # scale k
-            rx = d_x[d_idx]-d_x[d_idx-self.N]
-            ry = d_y[d_idx]-d_y[d_idx-self.N]
-            rz = d_z[d_idx]-d_z[d_idx-self.N]
-            k = self.k/sqrt(rx**2+ry**2+rz**2)
-            #k = self.k
+    def post_loop(self, d_idx, d_m, d_rho, d_x, d_y, d_z,
+                d_rxnext, d_rynext, d_rznext, d_rnext,
+                d_rxprev, d_ryprev, d_rzprev, d_rprev,
+                d_au, d_av, d_aw, d_omegax, d_omegay, d_omegaz,
+                d_testx, d_testy, d_testz, d_dudx, d_dudy, d_dudz, d_dvdx,
+                d_dvdy, d_dvdz, d_dwdx, d_dwdy, d_dwdz):
+        if d_rnext[d_idx] > 1E-14 and d_rprev[d_idx] > 1E-14:
+            mu = self.nu*d_rho[d_idx]
 
-            n = self.N+1
-            for idx in range(d_idx-self.N, d_idx+1):
-                xx += 1.0/n * d_x[idx]
-                yy += 1.0/n * d_y[idx]
-                zz += 1.0/n * d_z[idx]
-                ox += 1.0/n * d_omegax[idx]
-                oy += 1.0/n * d_omegay[idx]
-                oz += 1.0/n * d_omegaz[idx]
+            dx = d_rxnext[d_idx]-d_rxprev[d_idx]
+            dy = d_rynext[d_idx]-d_ryprev[d_idx]
+            dz = d_rznext[d_idx]-d_rzprev[d_idx]
+            r = sqrt(dx**2+dy**2+dz**2)
+            s1 = dx/r
+            s2 = dy/r
+            s3 = dz/r
 
-            for idx in range(d_idx-self.N, d_idx+1):
-                xab = d_x[idx]-xx
-                yab = d_y[idx]-yy
-                zab = d_z[idx]-zz
+            # ensuring that [sx sy sz] is not parallel to [1 0 0]
+            if abs(s2) > 1E-14 or abs(s3) > 1E-14:
+                fac = (self.A * self.d/2 * mu)/(s2**2+s3**2)
 
-                d_au[idx] += k*(oy*zab-oz*yab)
-                d_av[idx] += k*(oz*xab-ox*zab)
-                d_aw[idx] += k*(ox*yab-oy*xab)
+                Mx = fac*((s1*s2**2*s3+s1*s3**3)*d_dvdx[d_idx]
+                    +(-s1**2*s2*s3+s2*s3)*d_dvdy[d_idx]
+                    +(-s1**2*s3**2-s2**2)*d_dvdz[d_idx]
+                    +(-s1*s2**3-s1*s2*s3**2)*d_dwdx[d_idx]
+                    +(s1**2*s2**2+s3**2)*d_dwdy[d_idx]
+                    +(s1**2*s2*s3-s2*s3)*d_dwdz[d_idx])
+                My = fac*((-s1*s2**2*s3-s1*s3**3)*d_dudx[d_idx]
+                    +(s1**2*s2*s3-s2*s3)*d_dudy[d_idx]
+                    +(s1**2*s3**2+s2**2)*d_dudz[d_idx]
+                    +(-s2**4-2*s2**2*s3**2-s3**4)*d_dwdx[d_idx]
+                    +(s1*s2**3+s1*s2*s3**2)*d_dwdy[d_idx]
+                    +(s1*s2**2*s3+s1*s3**3)*d_dwdz[d_idx])
+                Mz = fac*((s1*s2**3+s1*s2*s3**2)*d_dudx[d_idx]
+                    +(-s1**2*s2**2-s3**2)*d_dudy[d_idx]
+                    +(-s1**2*s2*s3+s2*s3)*d_dudz[d_idx]
+                    +(s2**4+2*s2**2*s3**2+s3**4)*d_dvdx[d_idx]
+                    +(-s1*s2**3-s1*s2*s3**2)*d_dvdy[d_idx]
+                    +(-s1*s2**2*s3-s1*s3**3)*d_dvdz[d_idx])
+            else:
+                fac = (self.A * self.d/2 * mu)/(s1**2+s3**2)
 
-                # just for Debugging:
-                d_testx[idx] = d_au[idx]
-                d_testy[idx] = d_av[idx]
-                d_testz[idx] = d_aw[idx]
+                Mx = fac*((-s1*s2**2*s3+s1*s3)*d_dvdx[d_idx]
+                    +(s1**2*s2*s3+s2*s3**3)*d_dvdy[d_idx]
+                    +(-s2**2*s3**2-s1**2)*d_dvdz[d_idx]
+                    +(-s1**3*s2-s1*s2*s3**2)*d_dwdx[d_idx]
+                    +(s1**4+2*s1**2*s3**2+s3**4)*d_dwdy[d_idx]
+                    +(-s1**2*s2*s3-s2*s3**3)*d_dwdz[d_idx])
+                My = fac*((s1*s2**2*s3-s1*s3)*d_dudx[d_idx]
+                    +(-s1**2*s2*s3-s2*s3**3)*d_dudy[d_idx]
+                    +(s2**2*s3**2+s1**2)*d_dudz[d_idx]
+                    +(-s1**2*s2**2-s3**2)*d_dwdx[d_idx]
+                    +(s1**3*s2+s1*s2*s3**2)*d_dwdy[d_idx]
+                    +(-s1*s2**2*s3+s1*s3)*d_dwdz[d_idx])
+                Mz = fac*((s1**3*s2+s1*s2*s3**2)*d_dudx[d_idx]
+                    +(-s1**4-2*s1**2*s3**2-s3**4)*d_dudy[d_idx]
+                    +(s1**2*s2*s3+s2*s3**3)*d_dudz[d_idx]
+                    +(s1**2*s2**2+s3**2)*d_dvdx[d_idx]
+                    +(-s1**3*s2-s1*s2*s3**2)*d_dvdy[d_idx]
+                    +(s1*s2**2*s3-s1*s3)*d_dvdz[d_idx])
+
+            d_au[d_idx+1] -= (My*d_rznext[d_idx]-Mz*d_rynext[d_idx])/(2*self.J)
+            d_av[d_idx+1] -= (Mz*d_rxnext[d_idx]-Mx*d_rznext[d_idx])/(2*self.J)
+            d_aw[d_idx+1] -= (Mx*d_rynext[d_idx]-My*d_rxnext[d_idx])/(2*self.J)
+
+            d_au[d_idx-1] -= (My*d_rzprev[d_idx]-Mz*d_ryprev[d_idx])/(2*self.J)
+            d_av[d_idx-1] -= (Mz*d_rxprev[d_idx]-Mx*d_rzprev[d_idx])/(2*self.J)
+            d_aw[d_idx-1] -= (Mx*d_ryprev[d_idx]-My*d_rxprev[d_idx])/(2*self.J)
+            # just for debugging
+            d_testx[d_idx+1] -= (My*d_rznext[d_idx]-Mz*d_rynext[d_idx])/(2*self.J)
+            d_testy[d_idx+1] -= (Mz*d_rxnext[d_idx]-Mx*d_rznext[d_idx])/(2*self.J)
+            d_testz[d_idx+1] -= (Mx*d_rynext[d_idx]-My*d_rxnext[d_idx])/(2*self.J)
+
+            d_testx[d_idx-1] -= (My*d_rzprev[d_idx]-Mz*d_ryprev[d_idx])/(2*self.J)
+            d_testy[d_idx-1] -= (Mz*d_rxprev[d_idx]-Mx*d_rzprev[d_idx])/(2*self.J)
+            d_testz[d_idx-1] -= (Mx*d_ryprev[d_idx]-My*d_rxprev[d_idx])/(2*self.J)
+
+
+    # def post_loop(self, d_idx, d_m, d_rho, d_x, d_y, d_z,
+    #             d_rxnext, d_rynext, d_rznext, d_rnext,
+    #             d_rxprev, d_ryprev, d_rzprev, d_rprev,
+    #             d_au, d_av, d_aw, d_omegax, d_omegay, d_omegaz,
+    #             d_testx, d_testy, d_testz, d_dudx, d_dudy, d_dudz, d_dvdx,
+    #             d_dvdy, d_dvdz, d_dwdx, d_dwdy, d_dwdz):
+    #     if d_rnext[d_idx] > 1E-14 and d_rprev[d_idx] > 1E-14:
+    #         mu = self.nu*d_rho[d_idx]
+    #
+    #         dx = d_rxnext[d_idx]-d_rxprev[d_idx]
+    #         dy = d_rynext[d_idx]-d_ryprev[d_idx]
+    #         dz = d_rznext[d_idx]-d_rzprev[d_idx]
+    #         r = sqrt(dx**2+dy**2+dz**2)
+    #         epsilon = 0.001*r
+    #
+    #         phiz = atan(dy/(dx+epsilon))
+    #         phiy = atan(dz/(dx+epsilon))
+    #         phix = atan(dz/(dy+epsilon))
+    #
+    #         nx = sin(phiz) * sin(phiy)
+    #         ny = cos(phiz) * cos(phix)
+    #         nz = cos(phiy) * sin(phix)
+    #
+    #         tx = mu * (d_dudx[d_idx]*nx + d_dudy[d_idx]*ny + d_dudz[d_idx]*nz)
+    #         ty = mu * (d_dvdx[d_idx]*nx + d_dvdy[d_idx]*ny + d_dudz[d_idx]*nz)
+    #         tz = mu * (d_dwdx[d_idx]*nx + d_dwdy[d_idx]*ny + d_dwdz[d_idx]*nz)
+    #
+    #         Mx = self.A * self.d/2 * (ty*nz - tz*ny)
+    #         My = self.A * self.d/2 * (tz*nx - tx*nz)
+    #         Mz = self.A * self.d/2 * (tx*ny - ty*nx)
+    #
+    #         d_au[d_idx+1] += (My*d_rznext[d_idx]-Mz*d_rynext[d_idx])/(2*self.J)
+    #         d_av[d_idx+1] += (Mz*d_rxnext[d_idx]-Mx*d_rznext[d_idx])/(2*self.J)
+    #         d_aw[d_idx+1] += (Mx*d_rynext[d_idx]-My*d_rxnext[d_idx])/(2*self.J)
+    #
+    #         d_au[d_idx-1] += (My*d_rzprev[d_idx]-Mz*d_ryprev[d_idx])/(2*self.J)
+    #         d_av[d_idx-1] += (Mz*d_rxprev[d_idx]-Mx*d_rzprev[d_idx])/(2*self.J)
+    #         d_aw[d_idx-1] += (Mx*d_ryprev[d_idx]-My*d_rxprev[d_idx])/(2*self.J)
+    #
+    #         # just for debugging
+    #         d_testx[d_idx+1] += (My*d_rznext[d_idx]-Mz*d_rynext[d_idx])/(2*self.J)
+    #         d_testy[d_idx+1] += (Mz*d_rxnext[d_idx]-Mx*d_rznext[d_idx])/(2*self.J)
+    #         d_testz[d_idx+1] += (Mx*d_rynext[d_idx]-My*d_rxnext[d_idx])/(2*self.J)
+    #
+    #         d_testx[d_idx-1] += (My*d_rzprev[d_idx]-Mz*d_ryprev[d_idx])/(2*self.J)
+    #         d_testy[d_idx-1] += (Mz*d_rxprev[d_idx]-Mx*d_rzprev[d_idx])/(2*self.J)
+    #         d_testz[d_idx-1] += (Mx*d_ryprev[d_idx]-My*d_rxprev[d_idx])/(2*self.J)
+
+
+
+# class Friction(Equation):
+#     r"""**Fiber bending based on friction**
+#
+#     Rigid rotation based on friction. Does not work across periodic boundaries!
+#     """
+#
+#     def __init__(self, dest, sources, J, k, n=2):
+#         r"""
+#         Parameters
+#         ----------
+#         J : float
+#             moment of inertia
+#         k : float
+#             friction coefficient for torque from vorticity
+#         n : int
+#             rigid motion rod length
+#         """
+#         self.J = J
+#         self.k = k
+#         self.N = n-1
+#         self.hold_tag_count = 1
+#         super(Friction, self).__init__(dest, sources)
+#
+#     def initialize(self, d_idx, d_au, d_av, d_aw):
+#         d_au[d_idx] = 0.0
+#         d_av[d_idx] = 0.0
+#         d_aw[d_idx] = 0.0
+#
+#     def loop(self, d_idx, d_x, d_y, d_z, d_omegax, d_omegay, d_omegaz,
+#                 d_au, d_av, d_aw, d_m, d_tag, d_testx, d_testy, d_testz,
+#                 d_holdtag):
+#         if ((d_idx+self.hold_tag_count)%(self.N+1) == 0 and d_idx > 0):
+#             xx = 0.0; yy = 0.0; zz = 0.0
+#             Mx = 0.0; My = 0.0; Mz = 0.0
+#
+#             # temporary workaround for angles
+#             #ddx = d_x[d_idx-self.N] - d_x[d_idx+1]
+#             #ddy = d_y[d_idx-self.N] - d_y[d_idx+1]
+#             #phi = atan(ddx/(ddy+0.001*ddx))
+#
+#             n = self.N+1
+#             for idx in range(d_idx-self.N, d_idx+1):
+#                 xx += 1.0/n * d_x[idx]
+#                 yy += 1.0/n * d_y[idx]
+#                 zz += 1.0/n * d_z[idx]
+#                 # ox += 1.0/n * d_omegax[idx]
+#                 # oy += 1.0/n * d_omegay[idx]
+#                 # oz += 1.0/n * d_omegaz[idx]
+#                 Mx += 1.0/n*self.k*d_omegax[idx]
+#                 My += 1.0/n*self.k*d_omegay[idx]
+#                 Mz += 1.0/n*self.k*d_omegaz[idx]
+#
+#
+#             for idx in range(d_idx-self.N, d_idx+1):
+#                 xab = d_x[idx]-xx
+#                 yab = d_y[idx]-yy
+#                 zab = d_z[idx]-zz
+#
+#                 d_au[idx] += (My*zab-Mz*yab)/self.J
+#                 d_av[idx] += (Mz*xab-Mx*zab)/self.J
+#                 d_aw[idx] += (Mx*yab-My*xab)/self.J
+#
+#                 # just for Debugging:
+#                 if d_holdtag[idx] == 0:
+#                     d_testx[idx] = (My*zab-Mz*yab)/self.J
+#                     d_testy[idx] = (Mz*xab-Mx*zab)/self.J
+#                     d_testz[idx] = (Mx*yab-My*xab)/self.J
+#
+#         # handle holds
+#         if not d_holdtag[d_idx] == 0:
+#             self.hold_tag_count +=1
