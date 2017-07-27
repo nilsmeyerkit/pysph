5b6f2c06b947d9711f957b4e04b5abeb542fa927
Nils Meyer <nmeyer7@uwo.ca>
2017-07-27 08:06:13 -0400

    Modified forgacs batch to work on SHARCNET Cluster

--- batch/forgacs_batch.sh
+++ batch/forgacs_batch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# G = 1.0, 3.20, 3.54, 4.35
+G=1.0
+
 # setting up virtual python
 cd /home/nmeyer7/virtual_python
 source fake_venv.sh
 
 # changing to scratch directory
-cd /scratch/nmeyer7
+mkdir /scratch/nmeyer7/G=${G}
+cd /scratch/nmeyer7/G=${G}
 
 # running problem with openmp
-pysph run ebg.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G 1 --openmp
+pysph run ebg.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G ${G} --holdcenter --openmp
