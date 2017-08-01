a9a044ab92dd54c8965e4e3893035bbf565ca1b3
Nils Meyer <mail@meyernils.de>
2017-08-01 09:17:55 -0400

    Updated shell scripts for orca.

--- batch/forgacs_batch.sh
+++ batch/forgacs_batch.sh
@@ -12,4 +12,4 @@ mkdir /scratch/nmeyer7/G=${G}
 cd /scratch/nmeyer7/G=${G}
 
 # running problem with openmp
-sqsub -q threaded -n 4 -o ~/nmeyer7/G=${G} -r 8h --mpp 2G pysph run ebg.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G ${G} --holdcenter --openmp
+sqsub -q threaded -n 24 -o ~/nmeyer7/G=${G} -r 5d pysph run ebg.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G ${G} --holdcenter --openmp
--- batch/nearfield_batch.sh
+++ batch/nearfield_batch.sh
@@ -4,6 +4,10 @@ echo "#########################################"
 echo "#Running batch mode for nearfield model.#"
 echo "#########################################"
 
+# setting up virtual python
+cd /home/nmeyer7/virtual_python
+source fake_venv.sh
+
 # fluidres=1
 # pysph run ebg.nearfield --ar 20 --V 1E-4 --massscale 1E8 --fluidres $fluidres --pb
 # mv nearfield_output/ \
@@ -32,16 +36,19 @@ echo "#########################################"
 # ~/Dropbox/Thesis/Documentation/SPH/Nearfield/Res_${fluidres}/
 
 fluidres=1
-pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb
-mv nearfield_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Nearfield/Res_${fluidres}_poiseuielle/
+mkdir /scratch/nmeyer7/res${fluidres}
+cd /scratch/nmeyer7/res${fluidres}
+sqsub -o ~/nmeyer7/res${fluidres} -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb
+mv /scratch/nmeyer7/res${fluidres} ~/res${fluidres}
 
 fluidres=0.5
-pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
-mv nearfield_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Nearfield/Res_${fluidres}_poiseuielle/
+mkdir /scratch/nmeyer7/res${fluidres}
+cd /scratch/nmeyer7/res${fluidres}
+sqsub -q threaded -n 4 -o ~/nmeyer7/res${fluidres} -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
+mv /scratch/nmeyer7/res${fluidres} ~/res${fluidres}
 
 fluidres=0.25
-pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
-mv nearfield_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Nearfield/Res_${fluidres}_poiseuielle/
+mkdir /scratch/nmeyer7/res${fluidres}
+cd /scratch/nmeyer7/res${fluidres}
+sqsub -q threaded -n 16 -o ~/nmeyer7/res${fluidres} -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
+mv /scratch/nmeyer7/res${fluidres} ~/res${fluidres}
