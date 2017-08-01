b99d82f9804ebf141eec179ec1b36210801121fc
Nils Meyer <mail@meyernils.de>
2017-08-01 10:34:22 -0400

    Added memory requirement for cluster jobs.

--- batch/forgacs_batch.sh
+++ batch/forgacs_batch.sh
@@ -12,4 +12,4 @@ mkdir /scratch/nmeyer7/G=${G}
 cd /scratch/nmeyer7/G=${G}
 
 # running problem with openmp
-sqsub -q threaded -n 24 -o /home/nmeyer7/G=${G} -r 5d pysph run ebg.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G ${G} --holdcenter --openmp
+sqsub -q threaded -n 24 -o /home/nmeyer7/G=${G} -r 5d --mpp 2G pysph run ebg.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G ${G} --holdcenter --openmp
--- batch/nearfield_batch.sh
+++ batch/nearfield_batch.sh
@@ -34,14 +34,14 @@ source fake_venv.sh
 fluidres=1
 mkdir /scratch/nmeyer7/res${fluidres}
 cd /scratch/nmeyer7/res${fluidres}
-sqsub -o /home/nmeyer7/res${fluidres}.log -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb
+sqsub -o /home/nmeyer7/res${fluidres}.log -r 3h --mpp 2G pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb
 
 fluidres=0.5
 mkdir /scratch/nmeyer7/res${fluidres}
 cd /scratch/nmeyer7/res${fluidres}
-sqsub -q threaded -n 4 -o /home/nmeyer7/res${fluidres}.log -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
+sqsub -q threaded -n 4 -o /home/nmeyer7/res${fluidres}.log -r 3h --mpp 2G pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
 
 fluidres=0.25
 mkdir /scratch/nmeyer7/res${fluidres}
 cd /scratch/nmeyer7/res${fluidres}
-sqsub -q threaded -n 16 -o /home/nmeyer7/res${fluidres}.log -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
+sqsub -q threaded -n 16 -o /home/nmeyer7/res${fluidres}.log -r 3h --mpp 2G pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
