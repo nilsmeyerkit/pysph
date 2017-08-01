0cea3668ef47bdab26f72d89a4bef1297b8c100e
Nils Meyer <mail@meyernils.de>
2017-08-01 10:30:01 -0400

    Replaced ~ with explicit /home for cluster. SHARCNET doesn't like the ~.

--- batch/nearfield_batch.sh
+++ batch/nearfield_batch.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "#########################################"
-echo "#Running batch mode for nearfield model.#"
-echo "#########################################"
-
 # setting up virtual python
 cd /home/nmeyer7/virtual_python
 source fake_venv.sh
@@ -38,17 +34,14 @@ source fake_venv.sh
 fluidres=1
 mkdir /scratch/nmeyer7/res${fluidres}
 cd /scratch/nmeyer7/res${fluidres}
-sqsub -o ~/nmeyer7/res${fluidres} -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb
-mv /scratch/nmeyer7/res${fluidres} ~/res${fluidres}
+sqsub -o /home/nmeyer7/res${fluidres}.log -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb
 
 fluidres=0.5
 mkdir /scratch/nmeyer7/res${fluidres}
 cd /scratch/nmeyer7/res${fluidres}
-sqsub -q threaded -n 4 -o ~/nmeyer7/res${fluidres} -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
-mv /scratch/nmeyer7/res${fluidres} ~/res${fluidres}
+sqsub -q threaded -n 4 -o /home/nmeyer7/res${fluidres}.log -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
 
 fluidres=0.25
 mkdir /scratch/nmeyer7/res${fluidres}
 cd /scratch/nmeyer7/res${fluidres}
-sqsub -q threaded -n 16 -o ~/nmeyer7/res${fluidres} -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
-mv /scratch/nmeyer7/res${fluidres} ~/res${fluidres}
+sqsub -q threaded -n 16 -o /home/nmeyer7/res${fluidres}.log -r 3h pysph run ebg.nearfield --ar 20 --g 10 --massscale 1E8 --fluidres $fluidres --pb --openmp
