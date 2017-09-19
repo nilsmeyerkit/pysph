d22d2612d35bf27108752f7707fb1d0bac143a4f
Nils Meyer <mail@meyernils.de>
2017-09-18 23:00:54 -0400

    Updated batch for statistical analysis.

--- batch/statistics_batch.sh
+++ batch/statistics_batch.sh
@@ -4,12 +4,12 @@
 cd /home/nmeyer7/virtual_python
 source fake_venv.sh
 
-for ar in 11 21 31 41 51
+for ar in 31 41 51
 do
   # changing to scratch directory
   mkdir /scratch/nmeyer7/ar=${ar}
   cd /scratch/nmeyer7/ar=${ar}
 
   # running problem with openmp
-  sqsub -q threaded -n 24 -o /home/nmeyer7/ar=${ar}.log -r 1d --mpp 1.5G pysph run fiber.channel --ar ${ar} --rot 10 --holdcenter --openmp
+  sqsub -q threaded -n 16 -o /home/nmeyer7/ar=${ar}.log -r 10h --mpp 2.5G pysph run fiber.channel --ar ${ar} --rot 10 --holdcenter --openmp
 done
