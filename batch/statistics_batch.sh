bda00351fc94f92a8f04d4fa2fb2148606f36134
Nils Meyer <nmeyer7@uwo.ca>
2017-09-20 08:29:37 -0400

    Fixed error in statistical batch.

--- batch/statistics_batch.sh
+++ batch/statistics_batch.sh
@@ -10,7 +10,7 @@ do
   mkdir /scratch/nmeyer7/ar=${ar}
   cd /scratch/nmeyer7/ar=${ar}
 
-  D=(($ar*1000))
+  D=$(($ar*1000))
 
   # running problem with openmp
   sqsub -q threaded -n 16 -o /home/nmeyer7/ar=${ar}.log -r ${ar}h --mpp 2.5G pysph run fiber.channel --ar ${ar} --rot 10 --holdcenter --D ${D} --openmp
