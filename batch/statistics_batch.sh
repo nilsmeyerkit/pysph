a947684071da406039a2db58e4497939d1cb565a
Nils Meyer <mail@meyernils.de>
2017-09-21 07:37:46 -0400

    Increased Damping factor in statistical batch.

--- batch/statistics_batch.sh
+++ batch/statistics_batch.sh
@@ -4,13 +4,13 @@
 cd /home/nmeyer7/virtual_python
 source fake_venv.sh
 
-for ar in 11 21 31 41 51
+for ar in 11 21 31 41 51 61 81
 do
   # changing to scratch directory
   mkdir /scratch/nmeyer7/ar=${ar}
   cd /scratch/nmeyer7/ar=${ar}
 
-  D=$(($ar*1000))
+  D=$(($ar*2000))
 
   # running problem with openmp
   sqsub -q threaded -n 16 -o /home/nmeyer7/ar=${ar}.log -r ${ar}h --mpp 2.5G pysph run fiber.channel --ar ${ar} --rot 10 --holdcenter --D ${D} --openmp
