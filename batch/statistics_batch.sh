ab7d4d9b89e65cfd261b6f090c3029e2d280f95a
Nils Meyer <mail@meyernils.de>
2017-09-26 08:42:24 -0400

    Updated statistical batch to include mass scaling. Mass scaling was too big and viscous forces were not dominant anymore.

--- batch/statistics_batch.sh
+++ batch/statistics_batch.sh
@@ -4,14 +4,15 @@
 cd /home/nmeyer7/virtual_python
 source fake_venv.sh
 
-for ar in 11 21 31 41 51 61 81
+for ar in 11 21 31 41 51 61
 do
   # changing to scratch directory
   mkdir /scratch/nmeyer7/ar=${ar}
   cd /scratch/nmeyer7/ar=${ar}
 
-  D=$(($ar*2000))
+  D=$(($ar*1000))
+  M=$((1000000/$ar))
 
   # running problem with openmp
-  sqsub -q threaded -n 16 -o /home/nmeyer7/ar=${ar}.log -r ${ar}h --mpp 2.5G pysph run fiber.channel --ar ${ar} --rot 10 --holdcenter --D ${D} --openmp
+  sqsub -q threaded -n 16 -o /home/nmeyer7/ar=${ar}.log -r ${ar}h --mpp 2.5G pysph run fiber.channel --ar ${ar} --massscale ${M}--rot 10 --holdcenter --D ${D} --openmp
 done
