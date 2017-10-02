0d37cb5d498df36f3e1ae8e79983aea6d37959af
Nils Meyer <mail@meyernils.de>
2017-10-02 12:42:16 -0400

    Reduced number of samples in statistical batch.

--- batch/sph_batch.sh
+++ batch/sph_batch.sh
@@ -4,41 +4,46 @@ echo "#####################"
 echo "#Running batch mode.#"
 echo "#####################"
 
-echo "#### Young's modulus ####"
-
-echo "# Running modulus: 1E7 ... #"
-pysph run ebg.channel --ar 21 --E 1E07 --holdcenter --openmp
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E07/
-
-echo "# Running modulus: 1E8 ... #"
-pysph run ebg.channel --ar 21 --E 1E08 --holdcenter --openmp
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E08/
-
-echo "# Running modulus: 1E9 ... #"
-pysph run ebg.channel --ar 21 --E 1E09 --holdcenter --openmp
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E09/
+# echo "#### Young's modulus ####"
+#
+# echo "# Running modulus: 1E7 ... #"
+# pysph run ebg.channel --ar 21 --E 1E07 --holdcenter --openmp
+# mv channel_output/ \
+# ~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E07/
+#
+# echo "# Running modulus: 1E8 ... #"
+# pysph run ebg.channel --ar 21 --E 1E08 --holdcenter --openmp
+# mv channel_output/ \
+# ~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E08/
+#
+# echo "# Running modulus: 1E9 ... #"
+# pysph run ebg.channel --ar 21 --E 1E09 --holdcenter --openmp
+# mv channel_output/ \
+# ~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E09/
 
 echo "#### Aspect ratios ####"
 
-echo "Running aspect ratio: 11 ..."
-pysph run ebg.channel --ar 11 --holdcenter
+echo "Running aspect ratio: 5 ..."
+pysph run fiber.channel --ar 5 --holdcenter
 mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/11particles_E=1E11/
+~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/5particles/
+
+echo "Running aspect ratio: 11 ..."
+pysph run fiber.channel --ar 11 --holdcenter
+mv channel_output/ \
+~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/11particles/
 
 echo "Running aspect ratio: 21 ..."
-pysph run ebg.channel --ar 21 --holdcenter --openmp
+pysph run fiber.channel --ar 21 --holdcenter --openmp
 mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E11/
+~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles/
 
-echo "Running aspect ratio: 41 ..."
-pysph run ebg.channel --ar 41 --holdcenter --openmp
+echo "Running aspect ratio: 31 ..."
+pysph run fiber.channel --ar 31 --holdcenter --openmp
 mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/41particles_E=1E11/
+~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/31particles/
 
-echo "Running aspect ratio: 61 ..."
-pysph run ebg.channel --ar 61 --openmp
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/61particles_E=1E11/
+# echo "Running aspect ratio: 41 ..."
+# pysph run fiber.channel --ar 41 --holdcenter --openmp
+# mv channel_output/ \
+# ~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/41particles/
