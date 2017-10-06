a7da7321dd9b3bc0addfccbf5147c504dddaa88a
Nils Meyer <mail@meyernils.de>
2017-10-06 08:29:39 -0400

    Fixed Graham script.

--- batch/rve_batch.sh
+++ batch/rve_batch.sh
@@ -9,9 +9,9 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 for phi in 0.05 0.1
 do
   # changing to scratch directory
-  mkdir scratch/phi=${phi}
-  cd scratch/phi=${phi}
+  mkdir /scratch/nmeyer7/phi=${phi}
+  cd /scratch/nmeyer7/phi=${phi}
 
   # running problem with openmp
-  sbatch pysph run fiber.rve --dim 3 --volfrac ${phi} --ar 11 --massscale 1E8 --D 2 --folgartucker --openmp
+  sbatch pysph run fiber.rve --dim 3 --volfrac ${phi} --ar 11 --D 2 --folgartucker --openmp
 done
