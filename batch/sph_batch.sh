1fcc1da106aff6ee673b1318ca1b82c99506327e
Nils Meyer <mail@meyernils.de>
2017-10-28 11:50:04 -0400

    Modified SPH batch for Graham.

--- batch/sph_batch.sh
+++ batch/sph_batch.sh
@@ -1,49 +1,14 @@
 #!/bin/bash
+#SBATCH --account=def-hrymak-ab  # account name
+#SBATCH --mem-per-cpu=2G         # memory; default unit is megabytes
+#SBATCH --time=0-03:00           # time (DD-HH:MM)
+#SBATCH --output=%x-%j.out       # output log (<filename>-<jobid>.out)
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+export OMP_NUM_THREADS=16
 
-echo "#####################"
-echo "#Running batch mode.#"
-echo "#####################"
-
-# echo "#### Young's modulus ####"
-#
-# echo "# Running modulus: 1E7 ... #"
-# pysph run ebg.channel --ar 21 --E 1E07 --holdcenter --openmp
-# mv channel_output/ \
-# ~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E07/
-#
-# echo "# Running modulus: 1E8 ... #"
-# pysph run ebg.channel --ar 21 --E 1E08 --holdcenter --openmp
-# mv channel_output/ \
-# ~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E08/
-#
-# echo "# Running modulus: 1E9 ... #"
-# pysph run ebg.channel --ar 21 --E 1E09 --holdcenter --openmp
-# mv channel_output/ \
-# ~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles_E=1E09/
-
-echo "#### Aspect ratios ####"
-
-echo "Running aspect ratio: 5 ..."
-pysph run fiber.channel --ar 5 --holdcenter
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/5particles/
-
-echo "Running aspect ratio: 11 ..."
-pysph run fiber.channel --ar 11 --holdcenter
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/11particles/
-
-echo "Running aspect ratio: 21 ..."
-pysph run fiber.channel --ar 21 --holdcenter --openmp
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/21particles/
-
-echo "Running aspect ratio: 31 ..."
-pysph run fiber.channel --ar 31 --holdcenter --openmp
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/31particles/
-
-echo "Running aspect ratio: 41 ..."
-pysph run fiber.channel --ar 41 --holdcenter --openmp
-mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_Gleb/41particles/
+# changing to scratch directory
+mkdir /scratch/nmeyer7/channel_ar_$1
+cd /scratch/nmeyer7/channel_ar_$1
+pysph run fiber.channel --ar $1 --holdcenter --openmp $2
