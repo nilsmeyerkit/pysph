8c2c2f3ac84fb946c8212b670a3609ebce92c89f
Nils Meyer <mail@meyernils.de>
2017-10-30 14:53:55 -0400

    More rotations for RVE.

--- batch/rve_batch.sh
+++ batch/rve_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-hrymak-ab  # account name
 #SBATCH --mem-per-cpu=3G         # memory; default unit is megabytes
-#SBATCH --time=0-04:00           # time (DD-HH:MM)
+#SBATCH --time=0-20:00           # time (DD-HH:MM)
 #SBATCH --output=%x-%j.out       # output log (<filename>-<jobid>.out)
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -12,4 +12,4 @@ export OMP_NUM_THREADS=32
 mkdir /scratch/nmeyer7/ar20_phi_$1
 cd /scratch/nmeyer7/ar20_phi_$1
 # running problem with openmp
-pysph run fiber.rve --volfrac $1 --ar 20 --folgartucker --openmp
+pysph run fiber.rve --volfrac $1 --ar 20 --rot 10 --folgartucker --openmp
