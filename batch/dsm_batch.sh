6498ac01994c48a462520c807bb7564fc7f2902e
nils.meyer <nils.meyer@kit.edu>
2018-01-21 21:17:50 +0100

    Extended DSM Batch

--- batch/dsm_batch.sh
+++ batch/dsm_batch.sh
@@ -4,14 +4,18 @@ d=0.0001
 G=3.3
 mu=63
 
-pysph run fiber.channel --rot 2 --D 10000 --ar 17 --d $d --E $E --G $G --mu $mu --holdcenter --openmp --vtk
+pysph run fiber.channel --rot 2 --ar 11 --holdcenter --openmp --vtk
 mv channel_output/ \
-~/Dropbox/Thesis/Documentation/SPH/Shearflow_DSM/17particles/
+~/Dropbox/Thesis/Documentation/SPH/Shearflow_DSM/11particles/
 
-pysph run fiber.channel --rot 1 --D 10000 --ar 45 --d $d --E $E --G $G --mu $mu --openmp --vtk
+pysph run fiber.channel --rot 2 --ar 21 --holdcenter --openmp --vtk
+mv channel_output/ \
+~/Dropbox/Thesis/Documentation/SPH/Shearflow_DSM/21particles/
+
+pysph run fiber.channel --rot 1 --ar 45 --openmp --vtk
 mv channel_output/ \
 ~/Dropbox/Thesis/Documentation/SPH/Shearflow_DSM/45particles/
 
-pysph run fiber.channel --rot 1 --D 10000 --ar 81 --d $d --E $E --G $G --mu $mu --openmp --vtk
+pysph run fiber.channel --rot 1 --ar 81 --openmp --vtk
 mv channel_output/ \
 ~/Dropbox/Thesis/Documentation/SPH/Shearflow_DSM/81particles/
