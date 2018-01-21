2332d9423feedcc715b17fa68bc6bb16a4b5082e
nils.meyer <nils.meyer@kit.edu>
2018-01-21 19:37:12 +0100

    Reverted forgacs batch and using sph_batch for general submissions.

--- batch/forgacs_batch.sh
+++ batch/forgacs_batch.sh
@@ -3,12 +3,11 @@
 #SBATCH --mem-per-cpu=2G         # memory; default unit is megabytes
 #SBATCH --output=%x-%j.out       # output log (<filename>-<jobid>.out)
 #SBATCH --ntasks=32               # number of MPI processes
-##SBATCH --time=1-00:00           # time (DD-HH:MM)
+#SBATCH --time=3-00:00           # time (DD-HH:MM)
 
 # changing to scratch directory
-mkdir /scratch/nmeyer7/AR$1
-cd /scratch/nmeyer7/AR$1
+mkdir /scratch/nmeyer7/ForgacsG$1
+cd /scratch/nmeyer7/ForgacsG$1
 
 # running problem with openmp
-# pysph run fiber.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G $1 --holdcenter --openmp
-pysph run fiber.channel --ar $1 --E 1E11 --holdcenter --openmp
+pysph run fiber.channel --ar 171 --E 6.3E09 --d 0.0000122 --mu 9.12 --G $1 --holdcenter --openmp
--- batch/sph_batch.sh
+++ batch/sph_batch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #SBATCH --account=def-hrymak-ab  # account name
 #SBATCH --mem-per-cpu=2G         # memory; default unit is megabytes
-#SBATCH --time=0-03:00           # time (DD-HH:MM)
 #SBATCH --output=%x-%j.out       # output log (<filename>-<jobid>.out)
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-export OMP_NUM_THREADS=16
+#SBATCH --ntasks=32               # number of MPI processes
+
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=1
+##SBATCH --cpus-per-task=16
+#export OMP_NUM_THREADS=16
 
 # changing to scratch directory
-mkdir /scratch/nmeyer7/channel_ar_$1
-cd /scratch/nmeyer7/channel_ar_$1
-pysph run fiber.channel --ar $1 --holdcenter --openmp $2
+mkdir /scratch/nmeyer7/AR$1
+cd /scratch/nmeyer7/AR$1
+
+pysph run fiber.channel --ar $1 --E 1E11 --holdcenter --openmp
