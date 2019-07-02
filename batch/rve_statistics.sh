6daf46a2d0f870e825e70d525242add52915562a
Nils <nils.meyer@kit.edu>
2019-07-03 01:25:36 +0200

    Switched batch type.

--- batch/rve_statistics.sh
+++ batch/rve_statistics.sh
@@ -2,9 +2,11 @@
 
 volfrac=0.003
 
+
+
 for N in {1..10}
 do
   pysph run fiber.rve --volfrac $volfrac --openmp
-  mkdir sph-paper/results/volfrac1/${N}
-  cp -ar rve_output sph-paper/results/volfrac1/${N}
+  mkdir sph-paper/results/volfrac03/${N}
+  cp -ar rve_output sph-paper/results/volfrac03/${N}
 done
