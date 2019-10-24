fed622f98ae6219323deeb805df493f676aa2cb6
Nils <nils.meyer@kit.edu>
2019-10-24 18:44:30 +0200

    Removed old dim artefakt.

--- pysph/sph/scheme.py
+++ pysph/sph/scheme.py
@@ -820,15 +820,13 @@ class BeadChainScheme(Scheme):
 
         for solid in self.solids:
             g2.append(SetWallVelocity(dest=solid,
-                                      sources=self.fluids + self.fibers,
-                                      dim=self.dim))
+                                      sources=self.fluids + self.fibers))
 
         for fiber in self.fibers:
             g2.append(StateEquation(dest=fiber, sources=None, p0=self.p0,
                                     rho0=self.rho0, b=1.0))
             if self.viscous_fiber:
-                g2.append(SetWallVelocity(dest=fiber,
-                                          sources=self.fluids, dim=self.dim))
+                g2.append(SetWallVelocity(dest=fiber, sources=self.fluids))
 
         equations.append(Group(equations=g2, real=False))
 
